106ea41509d2182c9784ef0dbf85c3469bbf20be
Vimal Kumar .K.J <76762337+VimalKumarKJ@users.noreply.github.com>
2024-03-25 15:40:57 +0530

    Add files via upload

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ import axios from "axios";
 import bodyParser from "body-parser";
 
 const app = express();
-const port = 3000;
+const port = 5000;
 
 app.use(bodyParser.urlencoded({extended:true}));
 app.use(express.static("public"));
